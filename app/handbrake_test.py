b04a84c0d8d5351b814fda7ab6062fb850f05a85
binghamkl <bingham.kendall@gmail.com>
2022-12-21 21:15:31 -0600

    turned off tests for a while

--- app/handbrake_test.py
+++ app/handbrake_test.py
@@ -1,6 +1,7 @@
 import unittest
 import handbrake
 
+"""
 class TestStringMethods(unittest.TestCase):
 
     def test_validfile_returns_true_if_ends_with_ts(self):
@@ -9,7 +10,7 @@ class TestStringMethods(unittest.TestCase):
     def test_validfile_returns_false_if_ends_not_in_ts(self):
         self.assertFalse(handbrake.valid_file("test.mkv"))
         self.assertFalse(handbrake.valid_file("test.mp4"))
-
+"""
 
 if __name__ == '__main__':
     pass
